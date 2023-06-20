987dd4648f84ce945ce68cc1c15edba7694df5d9
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-06-20 17:33:43 +0300

    about1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Routes from "./components/Routes/Routes";
 import { BrowserRouter as Router } from "react-router-dom";
+import About from './components/About/About';
 
 function App() {
   return (
     <div>
        <Router>
         <Header/>
+        <About/>
       </Router>
     </div>
   );
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./About.scss";
+import aboutImg from '../../assets/1.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <div className='left'>
+        <h1 className='title'>Delicious Taste!</h1>
+        <p>The taste that everyone likes is here. Discover new organic drinks that refresh in the summer heat.</p>
+        <div className="search">
+
+        </div>
+      </div>
+      <div className="right">
+        <img src={aboutImg} alt="img" />;
+      </div>
+    </div>
   )
 }
 
