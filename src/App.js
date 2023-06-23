0b294d09d28027fd29371170668a7d718ce15333
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-06-23 12:14:07 +0300

    carousel

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@
 import './App.scss';
 import React from "react";
 import Header from "./components/Header/Header";
-import Routes from "./components/Routes/Routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import About from './components/About/About';
+import Carousel from './components/Carousel/Carousel'
 import data from "./components/data.json"
+
+
 function App() {
   return (
     <div>
        <Router>
         <Header/>
         <About/>
+        <Carousel/>
       </Router>
     </div>
   );
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "./Carousel.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import slide1 from "../../assets/1.jpg"
+import slide2 from "../../assets/2.jpg"
+import slide3 from "../../assets/3.jpg"
+import slide4 from "../../assets/1.jpg"
 
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "grey" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "grey" }}
+            onClick={onClick}
+        />
+    );
+}
 const Carousel = () => {
-  return (
-    <div>Carousel</div>
-  )
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        initialSlide: 0,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
+    };
+
+    return (
+        <div className="container">
+            <Slider {...settings}>
+                <div className="card">
+                
+                    <div className="card-body">
+                    <img src={slide1} alt="" />
+                        <h3>Slick</h3>
+                        <p>We use react slick</p>
+                    </div>
+                </div>
+                <div className="card">
+                   
+                    <div className="card-body">
+                    <img src={slide2} alt="" />
+                        <h3>Slick</h3>
+                        <p>We use react slick</p>
+                    </div>
+                </div>
+                <div className="card">
+                  
+                    <div className="card-body">
+                    <img src={slide3} alt="" />
+                        <h3>Slick</h3>
+                        <p>We use react slick</p>
+                    </div>
+                </div>
+                <div className="card">
+                  
+                    <div className="card-body">
+                    <img src={slide4} alt="" />
+                        <h3>Slick</h3>
+                        <p>We use react slick</p>
+                    </div>
+                </div>
+                <div className="card">
+                    
+                    <div className="card-body">
+                    <img src={slide1} alt="" />
+                        <h3>Slick</h3>
+                        <p>We use react slick</p>
+                    </div>
+                </div>
+
+            </Slider>
+        </div>
+    )
 }
 
 export default Carousel
